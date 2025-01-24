4d2b8acc959c44ff7e4eb591016a0ac5b29ec246
dharshinimanisekaran <dharshinimanisekaran8@gmail.com>
2025-01-24 14:25:36 +0530

    prgm2 file is added in branch2

---
+++ src/JavaPrograms/Prgm2.java
@@ -0,0 +1,9 @@
+package JavaPrograms;
+public class Prgm2
+     {
+        public static void main(String[] args) {
+            System.out.println("Dharshini");
+            
+        }
+    
+    }
